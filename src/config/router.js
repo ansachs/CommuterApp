b46e78d4e25fc238f629dd6b9311d0c46f6f8fdf
Raymond Aranda <raymond.aranda@gmail.com>
2018-04-02 20:15:11 -0500

    router add

---
+++ src/config/router.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+import DestinationForm from '../containers/DestinationForm.js';
+import CommuteOptions from '../containers/CommuteOptions.js';
+import RunningLate from '../containers/RunningLate.js';
+
+export const DestinationStack = StackNavigator({
+  DestinationForm: {
+    screen: DestinationForm,
+    navigationOptions: {
+      title: 'Destination',
+      header: null
+    }
+  },
+
+  CommuteOptions: {
+    screen: CommuteOptions,
+    navigationOptions: {
+      title: 'Commute Options',
+      header: null
+    }
+  },
+
+  UberPage: {
+    screen: UberPage,
+    navigationOptions: {
+      title: 'UberPage',
+      header: null
+    }
+  }
+});
+
+export const Tabs = TabNavigator({
+  DestinationForm2: {
+    screen: DestinationStack,
+    navigationOptions: {
+      tabBarLabel: 'Destination',
+      tabBarIcon: ({ tintColor }) => <Icon name="map" size={35} color={tintColor} />
+    }
+  },
+
+  CommuteOptions2: {
+    screen: CommuteOptions,
+    navigationOptions: {
+      tabBarLabel: 'Commute Options',
+      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
+    }
+  }
+},
+{
+  tabBarPosition: 'bottom'
+});
+
+export const RunningLateStack = StackNavigator({
+  RunningLate: {
+    screen: RunningLate,
+    navigationOptions: {
+      title: 'Running late?',
+      header: null
+    }
+  }
+})
+
+export const Root = StackNavigator({
+  Tabs: {
+    screen: Tabs,
+  },
+  RunningLate: {
+    screen: RunningLateStack
+  },
+}, {
+  mode: 'modal',
+  headerMode: 'none'
+});
