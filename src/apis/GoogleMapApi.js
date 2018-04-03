15a3cd5b409c7071bc74313a512d1a3688d7a8be
dannyt3355 <dtallen80@gmail.com>
2018-04-03 09:10:29 -0500

    latlong

--- src/apis/GoogleMapApi.js
+++ src/apis/GoogleMapApi.js
@@ -1,21 +1,26 @@
-const fetchModeByDrive = (startDestination, endDestination) => {
-  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestination}&destination=${endDestination}&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
+const convertToLatLong = (startDestination) => {
+  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${startDestination}&key=AIzaSyD2_6K7CF1C1ooSwgDxxDq2WBx8bAIihIU`)
     .then((response) => response.json())
 }
 
-const fetchModeByBicycling = (startDestination, endDestination) => {
-  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestination}&destination=${endDestination}&mode=bicycling&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
+const fetchModeByDrive = (startDestinationLat, startDestinationLng, endDestinationLat, endDestinationLng) => {
+  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestinationLat},${startDestinationLng}&destination=${endDestinationLat},${endDestinationLng}&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
     .then((response) => response.json())
 }
 
-const fetchModeByWalking = (startDestination, endDestination) => {
-  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestination}&destination=${endDestination}&mode=walking&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
+const fetchModeByBicycling = (startDestinationLat, startDestinationLng, endDestinationLat, endDestinationLng) => {
+  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestinationLat},${startDestinationLng}&destination=${endDestinationLat},${endDestinationLng}&mode=bicycling&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
+    .then((response) => response.json())
+}
+
+const fetchModeByWalking = (startDestinationLat, startDestinationLng, endDestinationLat, endDestinationLng) => {
+  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestinationLat},${startDestinationLng}&destination=${endDestinationLat},${endDestinationLng}&mode=walking&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
     .then((response) => response.json())
 }
 
 
-const fetchModeByTransit = (startDestination, endDestination) => {
-  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestination}&destination=${endDestination}&mode=transit&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
+const fetchModeByTransit = (startDestinationLat, startDestinationLng, endDestinationLat, endDestinationLng) => {
+  return fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startDestinationLat},${startDestinationLng}&destination=${endDestinationLat},${endDestinationLng}&mode=transit&key=AIzaSyAgKeNtylo0q2-5FEXPFV1EoGfDqXUSofE`)
     .then((response) => response.json())
 }
 
@@ -24,5 +29,6 @@ export default {
   fetchModeByDrive: fetchModeByDrive,
   fetchModeByBicycling: fetchModeByBicycling,
   fetchModeByWalking: fetchModeByWalking,
-  fetchModeByTransit: fetchModeByTransit
+  fetchModeByTransit: fetchModeByTransit,
+  convertToLatLong: convertToLatLong
 }
