9ffae088ad3450d0ed8d67fc5c358cf3b3198785
Raymond Aranda <raymond.aranda@gmail.com>
2018-04-09 08:59:38 -0500

    added uber request and fixed show pages

--- src/apis/UberApi.js
+++ src/apis/UberApi.js
@@ -1,4 +1,5 @@
 const serverToken = 'GWld7Zyxm1P2_7XEXl4y48aSy7OlWg0_E9CSKOdm'
+const accessToken = 'KA.eyJ2ZXJzaW9uIjoyLCJpZCI6IlFFeUlMYXlWVDhxNy9jNFJ0Vm4yZ2c9PSIsImV4cGlyZXNfYXQiOjE1MjU4MzkwMDgsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.xRR56s74ASmHv17h6BRn-p-QNJ-yZK7HKFOvQ2_epso'
 
 const getDriverEta = (serverToken, startLatitude, startLongitude) => {
   return fetch(`https://sandbox-api.uber.com/v1.2/estimates/time?start_latitude=${startLatitude}&start_longitude=${startLongitude}`,
@@ -47,8 +48,61 @@ const getDriverEtaToLocation = (serverToken, startDestinationLat, startDestinati
     });
 }
 
+const getFareId = (productId, startDestinationLat, startDestinationLng, endDestinationLat, endDestinationLng) => {
+  return fetch('https://sandbox-api.uber.com/v1.2/requests/estimate',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+      method: "POST",
+      body: JSON.stringify({
+        'product_id': productId,
+        'start_latitude': startDestinationLat,
+        'start_longitude': startDestinationLng,
+        'end_latitude': endDestinationLat,
+        'end_longitude': endDestinationLng
+      })
+    })
+}
+
+const requestRide = (fareId, productId, startDestinationLat, startDestinationLng, endDestinationLat, endDestinationLng) => {
+  return fetch('https://sandbox-api.uber.com/v1.2/requests',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        'fare_id': fareId,
+        'product_id': productId,
+        'start_latitude': startDestinationLat,
+        'start_longitude': startDestinationLng,
+        'end_latitude': endDestinationLat,
+        'end_longitude': endDestinationLng
+      })
+    })
+}
+
+const cancelRide = (requestId) => {
+  return fetch(`https://sandbox-api.uber.com/v1.2/requests/${requestId}`,
+    {
+      header: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+        'Accept-Language': 'en_US'
+      },
+      method: 'DELETE'
+    })
+  .then(response => response.json());
+}
+
 export default {
   serverToken: serverToken,
   getDriverEta: getDriverEta,
-  getDriverEtaToLocation: getDriverEtaToLocation
+  getDriverEtaToLocation: getDriverEtaToLocation,
+  getFareId: getFareId,
+  requestRide: requestRide,
+  cancelRide: cancelRide
 }
--- src/containers/UberPage.js
+++ src/containers/UberPage.js
@@ -1,34 +1,81 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapContainer from './MapContainer.js'
+import MapContainer from './MapContainer.js';
+import { Button } from 'react-native-elements';
+import UberApi from '../apis/UberApi.js'
 
 export default class uberPage extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDestination: this.props.navigation.state.params.startDestination,
+      endDestination: this.props.navigation.state.params.endDestination,
+      startLat: this.props.navigation.state.params.startLat,
+      startLng: this.props.navigation.state.params.startLng,
+      endLat: this.props.navigation.state.params.endLat,
+      endLng: this.props.navigation.state.params.endLng,
+      request_id: '',
+      status: ''
+    }
+  }
 
-    let startLat = this.props.navigation.state.params.startLat;
-    let startLng = this.props.navigation.state.params.startLng;
-    let endLat = this.props.navigation.state.params.endLat;
-    let endLng = this.props.navigation.state.params.endLng;
+  requestRide() {
+    let productId = '4bfc6c57-98c0-424f-a72e-c1e2a1d49939';
+    let startLat = this.state.startLat;
+    let startLng = this.state.startLng;
+    let endLat = this.state.endLat;
+    let endLng = this.state.endLng;
+
+    UberApi.getFareId(productId, startLat, startLng, endLat, endLng)
+    .then((response) => response.json())
+    .then((fareJson) => {
+      UberApi.requestRide(fareJson.fare.fare_id, productId, startLat, startLng, endLat, endLng)
+      .then((response) => response.json())
+      .then((requestJson) => {
+        console.log(requestJson)
+        this.setState({request_id: requestJson.request_id, status: requestJson.status})
+      })
+    })
+  }
+
+  cancelRide(requestId) {
+    requestId = this.state.request_id
+    console.log(requestId)
+    UberApi.cancelRide(requestId)
+  }
+
+  render() {
+    console.log(this.state.request_id)
+    console.log(this.state.status)
+    let { startDestination, endDestination, startLat, startLng, endLat, endLng } = this.state
 
     return (
-      <View style={styles.container}>
+      <View style={{flex:1}}>
         <MapContainer
+          startDestination={startDestination}
+          endDestination={endDestination}
           startLat={startLat}
           startLng={startLng}
           endLat={endLat}
           endLng={endLng}
           mode='driving'
         />
+        <View>
+          <Button
+            title='Request Ride'
+            onPress={this.requestRide.bind(this)}
+          />
+
+          <Button
+            title='Cancel Ride'
+            onPress={this.cancelRide.bind(this)}
+          />
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
